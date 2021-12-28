e3b73c46715e9edef8da957a498db1a83b5b368d
Victoria <85613749+s-torryy1819@users.noreply.github.com>
2021-12-28 22:52:37 +0200

    Create MainTriangle.java

---
+++ MainTriangle.java
@@ -0,0 +1,8 @@
+public class Main_triangle {
+    public static void main(String[] args){
+        Triangle t = new Triangle(3, 4, 5, 6);
+        t.findSquare();
+        t.findPerimeter();
+        t.areSimilar();
+    }
+}
